77fefb1ffe87f12c986dd2cd0ba16bb2bfa27671
Kane Cheshire <contactme@kanecheshire.com>
2021-02-15 22:18:05 +0000

    Correct type for TimeIntervals

--- Sources/FoundationExtension/TimeInterval/TimeInterval+Times.swift
+++ Sources/FoundationExtension/TimeInterval/TimeInterval+Times.swift
@@ -2,8 +2,8 @@ import Foundation
 
 public extension TimeInterval {
     
-    static let oneMinute = 60
-    static let fiveMinutes = 300
-    static let oneHour = 3_600
-    static let twentyFourHours = 86_400
+    static let oneMinute = 60.0
+    static let fiveMinutes = 300.0
+    static let oneHour = 3_600.0
+    static let twentyFourHours = 86_400.0
 }
